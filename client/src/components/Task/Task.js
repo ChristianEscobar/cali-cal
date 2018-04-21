c9164a8d5f282648d606552653f7a1d93655ecc0
alejandro-munoz <alejandro.z.munoz@gmail.com>
2018-04-21 12:56:24 -0700

    task modified

--- client/src/components/Task/Task.js
+++ client/src/components/Task/Task.js
@@ -7,21 +7,18 @@ export default class Task extends React.Component {
             width:300,
             margin:20,
           },
-          
-          
       }
     return (
       <div className='list-group' style={styles.container}>
-         <a href="#" className="list-group-item disabled">
+         <button  className="list-group-item disabled">
             Tasks
-        </a>
+        </button>
         {this.props.events.map(event => 
-            <a href="#" className='list-group-item ' key={event.name}>{event.startTime} <strong>{event.name}</strong>  </a>
-            // <div key={event.name} className="panel panel-default">
-            //     <div    className="panel panel-default" 
-            //             key={event.name}>{event.name}
-            //     </div>
-            // </div>
+            <button href=" " 
+                    className='list-group-item ' 
+                    key={event.name}>{event.startTime} 
+                    <strong>{event.name}</strong>  
+            </button>
         )}
       </div>
     )
