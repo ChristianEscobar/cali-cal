ead43e7ff7a7fd3de14aa2ea56d8bf617218f12d
elaine01 <ecumming.g@gmail.com>
2018-04-20 21:49:15 -0700

    added connection.js

---
+++ server/config/connection.js
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+// Dependencies
+const Sequelize = require("sequelize");
+
+// Creates mySQL connection using Sequelize
+const sequelize = new Sequelize("calendar_db", "root", "password", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+// Exports the connection for other files to use
+module.exports = sequelize;
