022c808f23d8dbbdd6cdca6c885b36b024afe928
elaine01 <ecumming.g@gmail.com>
2018-04-23 20:54:58 -0700

    updated reducer so it's ready for dynamic content changes

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -3,15 +3,22 @@ import { actionNames } from './actions';
 
 export const initialState = {
   days: {
-    0: { name: "Sunday", events: [
-        { name: 'Team meeting', startTime: '8:00am' }, 
-        { name: 'UAT definition', startTime: '3:00pm'}]},
-    1: { name: "Monday", events: [ {name: 'nap', startTime: '3:00pm'} ]},
-    2: { name: "Tuesday", events: [ { name: 'house party', startTime: '12:00pm'} ] },
-    3: { name: "Wednesday", events: [] },
-    4: { name: "Thursday", events: [ {name: 'house party', startTime: '8:00pm'} ] },
-    5: { name: "Friday", events: [] },
-    6: { name: "Saturday", events: [] },
+    // 0: { name: "Sunday", events: [
+    //     { name: 'Team meeting', startTime: '8:00am' }, 
+    //     { name: 'UAT definition', startTime: '3:00pm'}]},
+    // 1: { name: "Monday", events: [ {name: 'nap', startTime: '3:00pm'} ]},
+    // 2: { name: "Tuesday", events: [ { name: 'house party', startTime: '12:00pm'} ] },
+    // 3: { name: "Wednesday", events: [] },
+    // 4: { name: "Thursday", events: [ {name: 'house party', startTime: '8:00pm'} ] },
+    // 5: { name: "Friday", events: [] },
+    // 6: { name: "Saturday", events: [] },
+    0: [],
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
   },
   selectedDay: 0,
   savingStateStatus: 'unknown',
