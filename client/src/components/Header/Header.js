b009732581398d4eb24562816621e102b20a987b
elaine01 <ecumming.g@gmail.com>
2018-04-26 23:14:16 -0700

    updated header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './Header.css';
+import OauthLogin from '../OauthLogin';
 
 export default class Header extends React.Component {
   render() {
     return (
       <div className="header">
         <div>
-          <a href="">
-            <i className="fa fa-calendar" aria-hidden="true"></i>
-            Calendar
-          </a>
+          <div className="col-xs-6 col-sm-4">
+            <a href="">
+              <i className="fa fa-calendar" aria-hidden="true"></i>
+              Calendar
+            </a>
+          </div>
+          <div className="col-sm-5" />
+          <div className="col-xs-6 col-sm-3">
+            <OauthLogin />
+          </div>
         </div>
       </div>
     )
